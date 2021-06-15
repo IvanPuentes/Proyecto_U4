8a8008f5b3026af18e40c262c6e87d9477deccd7
Ivan Andres <ivan_andi@hotmail.com>
2021-06-15 17:12:03 -0500

    Proyecto de la unidad 4

---
+++ pages/orders/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.urls import path
+from orders.views import OrdersPageView, charge
+
+urlpatterns=[
+    path('charge/', charge, name='charge'),
+    path('orders/purchase/<int:pk>',OrdersPageView.as_view(), name='orders'),
+]
